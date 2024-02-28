Normalization corrected

src/modelTools.py
@@ -15,15 +15,12 @@ def set_gpu_gemory_growth():
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
         try:
-            # Currently, memory growth needs to be the same across GPUs
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
             print(e)
-
 
 def RESNetLayer(input_layer, filters, kernel_size):
     
@@ -31,10 +28,9 @@ def RESNetLayer(input_layer, filters, kernel_size):
     batch = BatchNormalization()(conv)
     relu = ReLU()(batch)    
     
-    conv = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding='same', activation=None)(relu)
-    batch = BatchNormalization()(conv)
-    relu = ReLU()(batch)
-
+    #conv = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding='same', activation=None)(relu)
+    #batch = BatchNormalization()(conv)
+    #relu = ReLU()(batch)
 
     add = Add()([input_layer, relu])
     relu = ReLU()(add)
@@ -51,16 +47,12 @@ def model_1(input_shape=None, num_classes=None, resnet_depth=50, resnet_filters=
     conv = Conv2D(filters=resnet_filters, kernel_size=kernel_size, strides=1, padding='same', activation=None)(inputs)
     batch = BatchNormalization()(conv)    
 
-    # Resnet50
+    # Resnet
     previous_layer = batch
-    for i in range(0,resnet_depth):
+    for i in range(0, resnet_depth):
         next_layer = RESNetLayer(previous_layer, filters=resnet_filters, kernel_size=kernel_size)
         previous_layer = next_layer
     
-    # Encoder
-        
-    # Decoder
-
     # Prediction
     outputs = Conv2D(filters=num_classes, kernel_size=kernel_size, strides=1, padding='same', activation='softmax')(previous_layer)
 

src/sen12ms_dataTools.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 from sen12ms_dataLoader import SEN12MSDataset, Seasons, LCBands, S1Bands, S2Bands
 
@@ -8,7 +10,7 @@ sen12ms_path = os.path.join(os.path.dirname(__file__), "..", "data")
 spring_only_path = os.path.join(os.path.dirname(__file__), "..", "data", "spring_only.txt")
 
 seasons = [Seasons.SPRING, Seasons.SUMMER, Seasons.FALL, Seasons.WINTER]
-classes = {
+colors = {
     1   : [230,25,75],      # Evergreen Needleleaf Forests
     2   : [60,180,75],      # Evergreen Broadleaf Forests
     3   : [255,225,25],     # Deciduous Needleleaf Forests
@@ -54,9 +56,7 @@ def sen2_color_image(image : np.array):
             color_idx = 0
             # loop over the bands in rgb order
             for k in [3,2,1]:
-                # Scale to the range of [-1 to 1]
-                out[i][j][color_idx] = image[i][j][k] / (2**15)
-                # TODO : Experiment with shifting by 2**15 and norming by w^16
+                out[i][j][color_idx] = image[i][j][k]
 
                 color_idx += 1
 
@@ -69,11 +69,11 @@ def igbp_color_image(image : np.array):
     for i in range(0,image.shape[0]):
         for j in range(0,image.shape[1]):
             # It seems like there is missing data for some of the images
-            if image[i][j] not in classes.keys():
+            if image[i][j] not in colors.keys():
                 print("missing pixel")
                 out[i][j] = [0, 0, 0]
             else:
-                out[i][j] = classes[image[i][j]]
+                out[i][j] = colors[image[i][j]]
     return out.astype(int)
 
 # Load a list of all the available files and return it
@@ -124,7 +124,7 @@ class SEN12MSDataTools():
         
         # Replace each label with a color
         # subplot all 3 next to eachother
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=[10,10])
+        fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=[15,5])
         
         # ax1 is a color sentinel 2 image
         ax1.imshow(sen2_color_image(x))
@@ -135,6 +135,15 @@ class SEN12MSDataTools():
         # ax3 is the predicted labels
         ax3.imshow(igbp_color_image(y_pred))
 
+        # Plot the legend for the labels
+        # Pack labels and colors into handles array
+        handles = []
+        for key in colors.keys():
+            color = (np.array(colors[key]) / 255.0)
+            name = names[key]
+            handles.append(Patch(color=color, label=name))
+        ax4.legend(handles=handles, loc='center', frameon=False)
+        ax4.axis('off')
 
         fig.tight_layout()
 

src/sen12ms_sequence.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import tensorflow as tf
 import progressbar
+from multiprocessing.pool import ThreadPool
 
 from sen12ms_dataLoader import SEN12MSDataset, Seasons, S1Bands, S2Bands, LCBands
 
@@ -37,7 +38,7 @@ def flatten_image(image : np.array):
     return out
 
 def normalize_image(image : np.array):
-    image = image.astype(float) / float(2**15)
+    image = image.astype(float) / float((2.0**16.0)-1.0)
     return image
 
 def reorder_image(image : np.array):
@@ -71,6 +72,36 @@ def onehot_encode(y : np.array):
                     y_onehot[i][j][k] = classes[y[i][j][k][0]]
 
     return y_onehot
+
+def _thread_calculate_class_weights(args):
+    class_weights = {}
+    for key in classes.keys():
+        class_weights[key-1] = 0
+
+    sen12ms : SEN12MSDataset = args[0]
+    data = args[1]
+
+    # Load triplet from index
+    lc = sen12ms.get_patch(
+        seasons[int(data[0])],
+        int(data[1]),
+        int(data[2]),
+        LCBands.IGBP)
+
+    # Get the counts of each class
+    unique, counts = np.unique(lc[0], return_counts=True)
+            
+    # Update the class weights with the counts
+    for i in range(0,len(unique)):
+        if unique[i]-1 in class_weights.keys():
+            class_weights[unique[i]-1] += counts[i]
+        else:
+            class_weights[unique[i]-1] = counts[i]
+
+    # Increase total samples by the number of pixels
+    samples = 256 * 256
+
+    return class_weights, samples
 
 class SEN12MSSequence(tf.keras.utils.Sequence):
 
@@ -113,42 +144,40 @@ class SEN12MSSequence(tf.keras.utils.Sequence):
         # Do any preprocessing steps on the batches
 
         
-        x = x
+        x = normalize_image(x)
         y = onehot_encode(y)
 
         return x, y
+
 
     def calculate_class_weights(self):
         self.class_weights = {}
         
         # Get all the patches, count occurences of pixel categories
         total_samples = 0
-        bar = progressbar.ProgressBar(0, self.data.shape[0] * 256 * 256, widgets=['[',progressbar.SimpleProgress(),']',progressbar.Percentage()])
-        for dataset_idx in range(0, self.data.shape[0]):
-            
-            # Load triplet from index
-            s1, s2, lc, bounds = self.sen12ms.get_s1s2lc_triplet(
-                seasons[int(self.data[dataset_idx][0])], 
-                int(self.data[dataset_idx][1]), 
-                int(self.data[dataset_idx][2]), 
-                s1_bands=S1Bands.ALL,
-                s2_bands=S2Bands.ALL, 
-                lc_bands=LCBands.IGBP)
+        bar = progressbar.ProgressBar(0, self.data.shape[0], widgets=['[',progressbar.SimpleProgress(),']',progressbar.Percentage()])
+        
+        # Create thread arg list
+        args = []
+        for i in range(0, self.data.shape[0]):
+            args.append((self.sen12ms, self.data[i][:]))
 
-            # Get the counts of each class
-            unique, counts = np.unique(lc[0], return_counts=True)
-            
-            # Update the class weights with the counts
-            for i in range(0,len(unique)):
-                if unique[i]-1 in self.class_weights.keys():
-                    self.class_weights[unique[i]-1] += counts[i]
-                else:
-                    self.class_weights[unique[i]-1] = counts[i]
+        # Multithreaded label counting
+        with ThreadPool(128) as pool:
+            for result in pool.imap(_thread_calculate_class_weights, args):
+                # Merge results
+                class_weights = result[0]
+                samples = result[1]
 
-            # Increase total samples by the number of pixels
-            total_samples += 256 * 256
+                for key in class_weights:
+                    if key in self.class_weights.keys():
+                        self.class_weights[key] += class_weights[key]
+                    else:
+                        self.class_weights[key] = 0
 
-            bar.update(total_samples)
+                total_samples += samples
+                
+                bar.update(bar.currval + 1)
 
         # Convert counts into class weights
         for key in self.class_weights.keys():